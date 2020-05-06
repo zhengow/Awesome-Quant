5bd4a06dba11400b69ccd08b4a1e75b597054fea
zhengow <38389586+zhengow@users.noreply.github.com>
2020-05-06 13:47:36 +0800

    Create data.py

---
+++ 04-Others/MultiFactors/Person/data.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+class Data():
+    
+    def __init__():
+        pass
+        #self.trade_date = self.get_trade_date(start_date, end_date)
+        
+    '''
+    def get_trade_date(start_date, end_date):
+        
+        date = pd.read_csv('date.csv')
+        return date.loc[start_date:end_date,:]
+    '''
+    
+    def get(data, startdate, enddate, hist = 0):
+        
+        if(data=='symbols'):
+            return pd.read_csv('symbols.csv', header=None)
+        
+        if(data=='date'):
+            start = 0
+            end = 0
+            df = pd.read_csv('date.csv')
+            for ii in range(df.shape[0]):
+                if(df['date'][ii]>startdate):
+                    start = ii
+                    break
+            for ii in range(start+1, df.shape[0]):
+                if(df['date'][ii]>enddate):
+                    end = ii
+                    break
+            print("load date is finished!")
+            return df['date'][start:end]
+        
+        try:
+            df = pd.read_csv(data+'.csv', index_col='date')
+            start = np.where(df.index == startdate)[0].tolist()[0]-hist
+            end = np.where(df.index == enddate)[0].tolist()[0]
+            print("load", data, "is finished!")
+            return df.iloc[start:end+1,:]
+        except Exception as e:
+            print(e)
